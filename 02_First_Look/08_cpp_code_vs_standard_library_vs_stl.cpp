090d623fb4962b5823fde028b4c10b2e3457b7b2
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2023-11-04 15:52:46 +0500

    cpp code vs libraries vs stl basic idea

---
+++ 02_First_Look/08_cpp_code_vs_standard_library_vs_stl.cpp
@@ -0,0 +1,33 @@
+# include <iostream>
+
+int main(int argc, char const *argv[]) {
+
+    // C++ Core Language Vs Standard Library Vs STL.
+    
+    // C++ Core Language Feature.
+        // C++ Core Language Feature is a basic building block that makes up the C++ programming.
+        // For Example:
+            // Defining the Variable.
+            // Calling a Function.
+            // Syntax stuff.
+            // etc...
+        // These core things are called C++ Core Language Feature.
+
+    // Standard Library.
+        // Standard Library is the library where some useful code is written which we can use in our Program.
+            // For Example: 
+                // iostream library is used to give us the cout, cin, features.
+                // strings library is used to provide us the string features.
+                // etc...
+    
+    // STL.
+        // STL is kind of collection of container types.
+        // We have algorithms and functions to work on these data types.
+        // We have specialized types that are called iterators that allow us to navigate through these containers.
+        // Potentially running these algorithms on each element in the collection.
+        // STL is highly specialized part of C++.
+            // For Example:
+
+
+    return 0;
+}
