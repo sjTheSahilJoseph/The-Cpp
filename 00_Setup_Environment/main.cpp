ee5f31080c986fa478f93083f02d0fa0dbd5f08d
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2023-12-12 13:37:12 +0500

    Update: Variables and Data Types

--- 00_Setup_Environment/main.cpp
+++ 00_Setup_Environment/main.cpp
@@ -2,7 +2,11 @@
 
 int main(int argc, char const *argv[]) {
 
-    // Setup the VS Code and Compiler to start C++ 20.
+    // Setup the Neovim and Compiler to start C++ 20.
+    // Compiler = G++;
+    // I'm using C++20;
+
+    // Compile Command = g++ filename.cpp -std=c++20 -o filename && ./filename
 
     return 0;
 }
